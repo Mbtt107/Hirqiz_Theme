cee5f495f361eea76e9201bb88005d180388f1e5
Seden Ecem <ttbpccorporation@outlook.com>
2021-07-28 12:53:21 +0300

    v13 beta 1

---
+++ upload/src/addons/HirqizAddon/Setup.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace HirqizAddon;
+
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+
+class Setup extends AbstractSetup
+{
+	use StepRunnerInstallTrait;
+	use StepRunnerUpgradeTrait;
+	use StepRunnerUninstallTrait;
+
+    /**
+     * ----------------
+     * 
+     * Hirqiz Addon
+     * 
+     * ----------------
+     */
+
+
+}
